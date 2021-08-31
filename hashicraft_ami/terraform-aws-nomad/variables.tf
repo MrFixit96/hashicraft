dee5290ed858190df58b678631011d88c4aead89
Tu Doan <tu@doan.tech>
2021-08-31 12:56:19 -0500

    Changed some lines

--- hashicraft_ami/terraform-aws-nomad/variables.tf
+++ hashicraft_ami/terraform-aws-nomad/variables.tf
@@ -22,7 +22,7 @@
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/nomad-consul-ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
   type        = string
-  default     = "ami-054b61f931e66cac4"
+  default     = "ami-08a687d784d4d0d57"
 }
 
 variable "cluster_name" {
